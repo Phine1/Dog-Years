4a8e215ea372c2b1acde897be1e196eab1fdc298
Phine1 <phinehas.sersah@amalitech.org>
2021-09-27 13:23:56 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+// I have set a variable equal to my age
+let myAge = 2;
+// another variable has been set to earlyYears
+let earlyYears = 2;
+earlyYears *=10.5;
+//2 has been subtracted from myAge variable.
+let laterYears = myAge-2;
+//laterYears variable has been multiplied by 4 to account for dog years.
+laterYears *= 4;
+console.log(laterYears);
+/*earlyYears variable has been added to the laterYears variable in the myageindodyears variable.*/
+let myAgeInDogYears = earlyYears+laterYears;
+let myName ='Mr. Kitty Cat'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${laterYears} years old in dog years`);
+
+
